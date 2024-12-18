3db83f5d57ecb053e762e07d58aa2684b8c29a7c
raj <razbc12@gmail.com>
2024-12-18 16:50:52 +0545

    Add Event Listeners For Buttons And Character Limit On Input Screen.

---
+++ main.js
@@ -0,0 +1,46 @@
+const numpad = document.querySelector(".numpad");
+const screen = document.querySelector("input");
+
+numpad.addEventListener("click", (event) => {
+    let num = event.target;
+    if(screen.value<=9999999999999999){
+    switch(num.id){
+        case '1':
+            screen.value += 1;
+            break;
+        case '2':
+            screen.value += 2;
+            break;
+        case '3':
+            screen.value += 3;
+            break;
+        case '4':
+            screen.value += 4;
+            break;
+        case '5':
+            screen.value += 5;
+            break;
+        case '6':
+            screen.value += 6;
+            break;
+        case '7':
+            screen.value += 7;
+            break;
+        case '8':
+            screen.value += 8;
+            break;
+        case '9':
+            screen.value += 9;
+            break;
+        case '0':
+            screen.value += 0;
+            break;
+        case 'equals':
+            // run function
+            break;
+        case ".":
+            // screen.value += ;
+            break;
+    }
+    }
+});
